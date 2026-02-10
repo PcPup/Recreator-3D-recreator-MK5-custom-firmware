9b142cc3371306e76ddce793b893592ac949bc56
samuel marten <147113447+PcPup@users.noreply.github.com>
2026-02-10 08:39:00 -0800

    Refactor configuration settings in Configuration.h
    cleaned up and removed unessary parts

--- Configuration.h
+++ Configuration.h
@@ -3,12 +3,9 @@
  * Created by PcPup on 2026-02-09
  */
 #pragma once
-
-#define CONFIG_EXAMPLES_DIR "Creality/Ender-3/CrealityV422"
-
 #define CONFIGURATION_H_VERSION 02010206
-
-#define STRING_CONFIG_H_AUTHOR "(PcPup, Recreator MK5, Creality 4.2.2)" 
+//For -Recreator MK5 -Creality 4.2.2
+#define STRING_CONFIG_H_AUTHOR "(PcPup -Recreator MK5 -Creality 4.2.2)" 
 #define SHOW_BOOTSCREEN
 #define SHOW_CUSTOM_BOOTSCREEN
 #define CUSTOM_STATUS_SCREEN_IMAGE
@@ -16,42 +13,27 @@
 #ifndef MOTHERBOARD
   #define MOTHERBOARD BOARD_CREALITY_V4
 #endif
-
-
 #define SERIAL_PORT 1
-
 #define BAUDRATE 115200
 #define CUSTOM_MACHINE_NAME "Recreator MK5"
-
-
 
 #define DISABLE_X true
 #define DISABLE_Y true
 #define DISABLE_Z true
+
 #define X_DRIVER_TYPE  A4988
 #define Y_DRIVER_TYPE  A4988
 #define Z_DRIVER_TYPE  A4988
 #define E0_DRIVER_TYPE A4988
+
 #define EXTRUDERS 1
+
 #define X_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define Y_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define Z_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define I_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define J_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define K_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define U_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define V_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define W_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define X_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define Y_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define Z_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define I_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define J_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define K_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define U_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define V_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define W_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define Z_MIN_PROBE_ENDSTOP_INVERTING false // Set to true to invert the logic of the probe.
 
 #define DEFAULT_NOMINAL_FILAMENT_DIA 1.75
 #define TEMP_SENSOR_0 1
@@ -65,7 +47,6 @@
 #define HEATER_0_MAXTEMP 275
 #define HOTEND_OVERSHOOT 15   
 
- 
 #define PIDTEMP           
 #define PID_MAX  255      
 #define PID_K1     0.95 
@@ -76,9 +57,7 @@
   #define DEFAULT_KD  76.55
 #endif
 
-
-#if ANY(PIDTEMP, PIDTEMPBED, PIDTEMPCHAMBER)
-  
+#if ANY(PIDTEMP)
   #define PID_FUNCTIONAL_RANGE 10                     
   #define PID_EDIT_MENU           
   #define PID_AUTOTUNE_MENU   
@@ -94,28 +73,22 @@
 #define USE_XMIN_PLUG
 #define USE_YMIN_PLUG
 #define USE_ZMIN_PLUG
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93 }
 
-
-#define DEFAULT_MAX_FEEDRATE          { 500, 500, 5, 25 }
-
-
-#define DEFAULT_MAX_ACCELERATION      { 500, 500, 100, 5000 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 0,0,0,93 }
+#define DEFAULT_MAX_FEEDRATE          { 0,0,0,25 }
+#define DEFAULT_MAX_ACCELERATION      { 0,0,0,5000 }
 
 #define DEFAULT_ACCELERATION           500    
 #define DEFAULT_RETRACT_ACCELERATION   500   
 #define DEFAULT_TRAVEL_ACCELERATION    500    
-
 #define DEFAULT_EJERK    5.0  
 
 #if DISABLED(CLASSIC_JERK)
   #define JUNCTION_DEVIATION_MM 0.08  
-  #define JD_HANDLE_SMALL_SEGMENTS    
-                                      
+  #define JD_HANDLE_SMALL_SEGMENTS                                   
 #endif
 
 #define S_CURVE_ACCELERATION
-
 
 #define X_ENABLE_ON 0
 #define Y_ENABLE_ON 0
@@ -128,7 +101,6 @@
 
 #define INVERT_E0_DIR true
 
-
 #define X_HOME_DIR -1
 #define Y_HOME_DIR -1
 #define Z_HOME_DIR -1
@@ -136,12 +108,11 @@
 #define X_BED_SIZE 0
 #define Y_BED_SIZE 0
 
-// Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
+#define X_MAX_POS 0
+#define Y_MAX_POS 0
 #define Z_MAX_POS 0
 #define MIN_SOFTWARE_ENDSTOPS
 #if ENABLED(MIN_SOFTWARE_ENDSTOPS)
@@ -158,10 +129,6 @@
   #define MAX_SOFTWARE_ENDSTOP_Z
 #endif
 
-#if EITHER(MIN_SOFTWARE_ENDSTOPS, MAX_SOFTWARE_ENDSTOPS)
-  //#define SOFT_ENDSTOPS_MENU_ITEM  // Enable/Disable software endstops from the LCD
-#endif
-
 #define HOMING_FEEDRATE_MM_M { (20*60), (20*60), (4*60) }
 
 #define VALIDATE_HOMING_ENDSTOPS
@@ -175,11 +142,9 @@
   //#define EEPROM_INIT_NOW   // Init EEPROM on first boot after a new build.
 #endif
 
-
 #define HOST_KEEPALIVE_FEATURE        // Disable this if your host doesn't like keepalive messages
 #define DEFAULT_KEEPALIVE_INTERVAL 2  // Number of seconds between "busy" messages. Set with M113.
 #define BUSY_WHILE_HEATING            // Some hosts require "busy" messages even during heating
-
 
 #define PREHEAT_1_LABEL       "PET1"
 #define PREHEAT_1_TEMP_HOTEND 210
@@ -189,12 +154,10 @@
 #define PREHEAT_2_TEMP_HOTEND 250
 #define PREHEAT_2_FAN_SPEED   0
 
-
 #define PRINTJOB_TIMER_AUTOSTART
 
 #define LCD_LANGUAGE en
 #define DISPLAY_CHARSET_HD44780 JAPANESE
-
 
 #define LCD_INFO_SCREEN_STYLE 0
 
